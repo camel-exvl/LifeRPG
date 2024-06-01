33ee5a0a1960166d29b0679d84437051f1c5653e
Camel <camel_exvl@outlook.com>
2024-06-01 13:50:57 +0800

    feat(target): 实现插入默认数据函数
    1. 实现task和habit插入默认数据的函数，用于程序初次启动无数据时调用
    2. 实现task和habit的viewmodel单例模式
    3. 优化task和habit的viewmodel相关函数，改为全异步且修改触发listener的时机，优化性能

--- lib/viewmodel/habit_viewmodel.dart
+++ lib/viewmodel/habit_viewmodel.dart
@@ -2,20 +2,78 @@ import 'dart:collection';
 
 import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:liferpg/database/database.dart';
 
+import '../model/common_model.dart';
+import '../model/habit_model.dart';
+
 class HabitViewModel extends ChangeNotifier {
+  static final HabitViewModel instance = HabitViewModel._internal();
+
+  HabitViewModel._internal();
+
+  factory HabitViewModel() => instance;
+
   final database = AppDatabase();
   List<HabitModel> _habits = [];
 
   UnmodifiableListView<HabitModel> get habits => UnmodifiableListView(_habits);
+
+  Future<void> initOnFirstRun(BuildContext context) async {
+    var habits = [
+      // learning
+      HabitTableCompanion(
+        order: const Value(0),
+        title: Value(AppLocalizations.of(context)!.defaultHabitReadingTitle),
+        description:
+            Value(AppLocalizations.of(context)!.defaultHabitReadingDescription),
+        difficulty: const Value(Difficulty.easy),
+        category: const Value(Category.learning),
+        type: const Value(HabitType.good),
+        finishedCount: const Value(0),
+        lastFinishedAt: Value(DateTime(0)),
+        createdAt: Value(DateTime.now()),
+      ),
+      // art
+      HabitTableCompanion(
+        order: const Value(1),
+        title: Value(AppLocalizations.of(context)!.defaultHabitPlayGuitarTitle),
+        description: Value(
+            AppLocalizations.of(context)!.defaultHabitPlayGuitarDescription),
+        difficulty: const Value(Difficulty.medium),
+        category: const Value(Category.art),
+        type: const Value(HabitType.good),
+        finishedCount: const Value(0),
+        lastFinishedAt: Value(DateTime(0)),
+        createdAt: Value(DateTime.now()),
+      ),
+      // health
+      HabitTableCompanion(
+        order: const Value(2),
+        title: Value(
+            AppLocalizations.of(context)!.defaultHabitSmokeCigaretteTitle),
+        description: const Value(""),
+        difficulty: const Value(Difficulty.hard),
+        category: const Value(Category.health),
+        type: const Value(HabitType.bad),
+        finishedCount: const Value(0),
+        lastFinishedAt: Value(DateTime(0)),
+        createdAt: Value(DateTime.now()),
+      ),
+    ];
+
+    for (var habit in habits) {
+      await database.insertHabit(habit);
+    }
+  }
 
   Future<void> loadHabits() async {
     _habits = await database.getAllHabits();
     notifyListeners();
   }
 
-  void insertHabit(HabitModel habit) {
+  Future<void> insertHabit(HabitModel habit) async {
     database
         .insertHabit(HabitTableCompanion(
       order: Value(habit.order),
@@ -33,29 +91,28 @@ class HabitViewModel extends ChangeNotifier {
       _habits.add(habit);
       notifyListeners();
     });
-    notifyListeners();
   }
 
-  void updateHabit(HabitModel habit) {
+  Future<void> updateHabit(HabitModel habit) async {
     _habits[_habits.indexWhere((element) => element.id == habit.id)] = habit;
+    notifyListeners();
     database.updateHabit(habit);
-    notifyListeners();
   }
 
-  void removeHabit(HabitModel habit) {
+  Future<void> removeHabit(HabitModel habit) async {
     _habits.remove(habit);
-    database.deleteHabit(habit);
     notifyListeners();
+    database.deleteHabit(habit);
   }
 
-  void reorderHabits(int oldIndex, int newIndex) {
+  Future<void> reorderHabits(int oldIndex, int newIndex) async {
     if (oldIndex < newIndex) {
       newIndex -= 1;
     }
     final habit = _habits.removeAt(oldIndex);
     _habits.insert(newIndex, habit);
+    notifyListeners();
 
     database.reorderHabits(_habits, oldIndex, newIndex);
-    notifyListeners();
   }
 }
--- lib/viewmodel/task_viewmodel.dart
+++ lib/viewmodel/task_viewmodel.dart
@@ -2,20 +2,87 @@ import 'dart:collection';
 
 import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:liferpg/database/database.dart';
 
+import '../model/common_model.dart';
+import '../model/task_model.dart';
+
 class TaskViewModel extends ChangeNotifier {
+  static final TaskViewModel instance = TaskViewModel._internal();
+
+  TaskViewModel._internal();
+
+  factory TaskViewModel() => instance;
+
   final database = AppDatabase();
   List<TaskModel> _tasks = [];
 
   UnmodifiableListView<TaskModel> get tasks => UnmodifiableListView(_tasks);
+
+  Future<void> initOnFirstRun(BuildContext context) async {
+    var tasks = [
+      // health
+      TaskTableCompanion(
+        order: const Value(0),
+        title: Value(AppLocalizations.of(context)!.defaultTaskJoggingTitle),
+        description:
+            Value(AppLocalizations.of(context)!.defaultTaskJoggingDescription),
+        difficulty: const Value(Difficulty.easy),
+        category: const Value(Category.health),
+        repeatType: const Value(RepeatType.daily),
+        repeatValue: const Value(1),
+        repeatDays: const Value([]),
+        deadline: const Value(null),
+        finishedCount: const Value(0),
+        lastFinishedAt: Value(DateTime(0)),
+        createdAt: Value(DateTime.now()),
+      ),
+      // learning
+      TaskTableCompanion(
+        order: const Value(1),
+        title: Value(AppLocalizations.of(context)!.defaultTaskReadBookTitle),
+        description: const Value(""),
+        difficulty: const Value(Difficulty.medium),
+        category: const Value(Category.learning),
+        repeatType: const Value(RepeatType.weekly),
+        repeatValue: const Value(1),
+        repeatDays: const Value([0, 6]),
+        deadline: const Value(null),
+        finishedCount: const Value(0),
+        lastFinishedAt: Value(DateTime(0)),
+        createdAt: Value(DateTime.now()),
+      ),
+      // career
+      TaskTableCompanion(
+        order: const Value(2),
+        title:
+            Value(AppLocalizations.of(context)!.defaultTaskFinishProjectTitle),
+        description: Value(
+            AppLocalizations.of(context)!.defaultTaskFinishProjectDescription),
+        difficulty: const Value(Difficulty.hard),
+        category: const Value(Category.career),
+        repeatType: const Value(RepeatType.none),
+        repeatValue: const Value(0),
+        repeatDays: const Value([]),
+        deadline: Value(DateTime.now().add(const Duration(days: 7))),
+        finishedCount: const Value(0),
+        lastFinishedAt: Value(DateTime(0)),
+        createdAt: Value(DateTime.now()),
+      ),
+    ];
+
+    for (var task in tasks) {
+      await database.insertTask(task);
+    }
+  }
 
   Future<void> loadTasks() async {
     _tasks = await database.getAllTasks();
     notifyListeners();
   }
 
-  void insertTask(TaskModel task) {
+  Future<void> insertTask(TaskModel task) async {
     database
         .insertTask(TaskTableCompanion(
       order: Value(task.order),
@@ -36,29 +103,28 @@ class TaskViewModel extends ChangeNotifier {
       _tasks.add(task);
       notifyListeners();
     });
-    notifyListeners();
   }
 
-  void updateTask(TaskModel task) {
+  Future<void> updateTask(TaskModel task) async {
     _tasks[_tasks.indexWhere((element) => element.id == task.id)] = task;
+    notifyListeners();
     database.updateTask(task);
-    notifyListeners();
   }
 
-  void removeTask(TaskModel task) {
+  Future<void> removeTask(TaskModel task) async {
     _tasks.remove(task);
-    database.deleteTask(task);
     notifyListeners();
+    database.deleteTask(task);
   }
 
-  void reorderTasks(int oldIndex, int newIndex) {
+  Future<void> reorderTasks(int oldIndex, int newIndex) async {
     if (oldIndex < newIndex) {
       newIndex -= 1;
     }
     final task = _tasks.removeAt(oldIndex);
     _tasks.insert(newIndex, task);
+    notifyListeners();
 
     database.reorderTasks(_tasks, oldIndex, newIndex);
-    notifyListeners();
   }
 }
