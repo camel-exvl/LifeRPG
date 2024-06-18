e9d4c3e8486e09000b0501321ef6ff101b85f329
Camel <58109197+camel-exvl@users.noreply.github.com>
2024-06-18 11:44:01 +0800

    docs: 修改部分文本与数值 (#39)
    1. 修改onboarding页面文本内容，使之更可读
    2. 调整商店部分物品售价，避免出现更贵的商品效果更差情况

--- lib/view/store/store_view.dart
+++ lib/view/store/store_view.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:liferpg/database/database.dart';
 import 'package:liferpg/model/challenge/challenge_model.dart';
 import 'package:liferpg/model/common_model.dart';
 import 'package:liferpg/model/store/equipment_model.dart';
